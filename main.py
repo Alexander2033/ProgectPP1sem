b40d3a55c3677cf7ebca34ad04f96846e3ff749e
Alexander2033 <tebin04@mail.ru>
2023-04-17 21:54:20 +0300

    version 2 (создал наконец-то базу данных)

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
-from flask import Flask, render_template
-#from flask_sqlachemy import SQLAlchemy
-#from datetime import datetime
+from flask import Flask, render_template, request
+from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
+# Create a connection to your database
+engine = create_engine('sqlite:///Based.db')
+
+# Create a session factory
+Session = sessionmaker(bind=engine)
+
+# Create a base class for your models
+Base = declarative_base()
+
+# Define a model
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Integer)
+    size = Column(Integer)
+
+# Create the table in the database
+Base.metadata.create_all(engine)
 
 app = Flask(__name__)
-
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blr.db'
 #db = SQLAlchemy(app)
 
 
-#class Article(db.Model):
- #   id = db.Column(db.Integer, primary_key=True)
-  #  title = db.Column(db.String(100), primary_key=False)
-   # intro = db.Column(db.String(300), primary_key=False)
-   # text = db.Column(db.Text, nullable=False)
-    #date = db.Column(db.DateTime, default=datetime.utcnow)
 
-#    def __repr__(self):
- #       return '<Article %r>' % self.id
 
 @app.route('/')
 def index():
@@ -35,6 +48,30 @@ def support():
 @app.route('/backet')
 def backet():
     return render_template('backet.html')
+
+
+@app.route('/creat-article', methods=['POST', 'GET'])
+def creat_article():
+    if (request.method == "POST"):
+        namef = request.form['name']
+        pricef = request.form['price']
+        sizef = request.form['size']
+        user = User(name=namef, price=pricef, size=sizef)
+
+        # Add the user to the database
+        session = Session()
+        session.add(user)
+        session.commit()
+
+        # Query the database for all users
+        users = session.query(User).all()
+
+        # Print the users
+        for user in users:
+            print(user.name)
+            print(user.price)
+            print(user.size)
+    return render_template('creat-article.html')
 
 
 if __name__ == "__main__":
