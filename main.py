f6c2ebececd8fe962d1f079018dbdfed160d1766
Alexander2033 <tebin04@mail.ru>
2023-04-19 19:33:09 +0300

    version 2 (создал наконец-то базу данных)

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 from datetime import datetime
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, ClauseList, ForeignKey
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 # Create a connection to your database
-engine = create_engine('sqlite:///Based.db')
+engine = create_engine('sqlite:///Baseee.db')
 
 # Create a session factory
 Session = sessionmaker(bind=engine)
@@ -14,13 +15,34 @@ Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
 # Define a model
+
+
+class Items(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    cost_price = Column(Integer)
+    quantity = Column(Integer)
+
+
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    price = Column(Integer)
-    size = Column(Integer)
+    telephone = Column(Integer)
+    password = Column(String)
+    email = Column(String)
+
+class Order(Base):
+    __tablename__ = 'order_lines'
+
+
+    id = Column(Integer, primary_key=True)
+    order_id = Column(ForeignKey('orders.id'))
+    item_id = Column(ForeignKey('items.id'))
+    quantity = Column(Integer)
 
 # Create the table in the database
 Base.metadata.create_all(engine)
@@ -35,6 +57,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/door', methods=['POST', 'GET'])
+def door():
+    if (request.method == "POST"):
+        passwordf = request.form['password']
+        emailf = request.form['email']
+        if (passwordf and emailf):
+            session1 = Session()
+            user = session1.query(User).filter_by(email=emailf).first()
+            if user:
+                if ((user.password == passwordf) and (user.email == emailf)):
+                    session['name'] = user.name
+                    print("True")
+                return redirect('/')
+        print("False")
+        return render_template('door.html')
+
+    else:
+        return render_template('door.html')
 
 @app.route('/katalog')
 def katalog():
@@ -42,6 +82,8 @@ def katalog():
 
 @app.route('/support')
 def support():
+    if 'name' in session:
+        print(session['name'])
     return render_template('Support.html')
 
 
@@ -49,31 +91,50 @@ def support():
 def backet():
     return render_template('backet.html')
 
+@app.route('/unsetsession', methods=['POST', 'GET'])
+def unsetsession():
+    if 'name' in session:
+        if (request.method == "POST"):
+            session.pop('name', None)
+            return redirect('/')
+        return render_template('unsetsession.html')
+    return render_template('Error2.html')
+
+
 
 @app.route('/creat-article', methods=['POST', 'GET'])
 def creat_article():
+    if 'name' in session:
+        return render_template('Error1.html')
     if (request.method == "POST"):
         namef = request.form['name']
-        pricef = request.form['price']
-        sizef = request.form['size']
-        user = User(name=namef, price=pricef, size=sizef)
+        telephonef = request.form['telephone']
+        passwordf = request.form['password']
+        emailf = request.form['email']
+        if (passwordf and emailf and telephonef and namef):
+            user = User(name=namef, telephone=telephonef, email=emailf, password=passwordf)
 
-        # Add the user to the database
-        session = Session()
-        session.add(user)
-        session.commit()
+            # Add the user to the database
+            session1 = Session()
+            session1.add(user)
+            session1.commit()
+            session['name'] = namef
+            # Query the database for all users
+            users = session1.query(User).all()
 
-        # Query the database for all users
-        users = session.query(User).all()
-
-        # Print the users
-        for user in users:
-            print(user.name)
-            print(user.price)
-            print(user.size)
-    return render_template('creat-article.html')
+            # Print the users
+            for user in users:
+                print(user.name)
+                print(user.telephone)
+                print(user.email)
+                print(user.password)
+            return redirect('/')
+        return render_template('creat-article.html')
+    else:
+        return render_template('creat-article.html')
 
 
 if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
     app.run(debug=True)
 
